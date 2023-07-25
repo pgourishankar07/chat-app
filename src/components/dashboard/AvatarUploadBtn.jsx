c6fd323103fbf05cdac3175848e4864c6728cf73
pgourishankar07 <100337318+pgourishankar07@users.noreply.github.com>
2023-07-25 23:46:17 +0530

    Storing Avatar in firebase storage

--- src/components/dashboard/AvatarUploadBtn.jsx
+++ src/components/dashboard/AvatarUploadBtn.jsx
@@ -1,15 +1,32 @@
 import { Alert, Button, Modal } from 'rsuite';
 import { useModalState } from '../../misc/customHooks';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import AvatarEditor from 'react-avatar-editor';
+import { useProfile } from '../../context/profile.context';
+import { database, storage } from '../../misc/firebase';
 
 const fileInputTypes = '.png,.jpeg,.jpg';
 const acceptFile = ['image/png', 'image/jpeg', 'image/pjpeg'];
 const isValid = file => acceptFile.includes(file.type);
 
+function getBlob(canvas) {
+  return new Promise((res, rej) => {
+    canvas.toBlob(blob => {
+      if (blob) {
+        res(blob);
+      } else {
+        rej(new Error('File process error'));
+      }
+    });
+  });
+}
+
 export default function AvatarUploadBtn() {
   const { isOpen, open, close } = useModalState();
   const [img, setImg] = useState(null);
+  const avatarEditorRef = useRef();
+  const { profile } = useProfile();
+  const [isLoading, setIsloading] = useState(false);
 
   function onFileTypeChange(event) {
     const currFiles = event.target.files;
@@ -22,6 +39,31 @@ export default function AvatarUploadBtn() {
       } else {
         Alert.warning(`Wrong file format ${file.type}`, 4000);
       }
+    }
+  }
+
+  async function onUploadClick() {
+    const canvas = avatarEditorRef.current.getImageScaledToCanvas();
+
+    try {
+      setIsloading(true);
+      const blob = await getBlob(canvas);
+      const avatarFileRef = storage
+        .ref(`/profile/${profile.uid}`)
+        .child('avatar');
+      const uploadAvatarRes = await avatarFileRef.put(blob, {
+        cacheControl: `public,max-age=${3600 * 24 * 3}`,
+      });
+      const downloadURL = await uploadAvatarRes.ref.getDownloadURL();
+      const userAvatarRef = database
+        .ref(`/profiles/${profile.uid}`)
+        .child('avatar');
+      userAvatarRef.set(downloadURL);
+      Alert.info('Avatar has been uploaded', 4000);
+      setIsloading(false);
+    } catch (err) {
+      setIsloading(false);
+      Alert.error(err.message, 4000);
     }
   }
 
@@ -49,6 +91,7 @@ export default function AvatarUploadBtn() {
             <div className="d-flex justify-content-center align-items-center h-100">
               {img && (
                 <AvatarEditor
+                  ref={avatarEditorRef}
                   image={img}
                   width={200}
                   height={200}
@@ -60,7 +103,12 @@ export default function AvatarUploadBtn() {
             </div>
           </Modal.Body>
           <Modal.Footer>
-            <Button block appearance="ghost">
+            <Button
+              block
+              appearance="ghost"
+              onClick={onUploadClick}
+              disabled={isLoading}
+            >
               Upload
             </Button>
           </Modal.Footer>
--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
+import 'firebase/storage';
 // Import the functions you need from the SDKs you need
 // import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js';
 // TODO: Add SDKs for Firebase products that you want to use
@@ -21,3 +22,4 @@ const firebaseConfig = {
 const app = firebase.initializeApp(firebaseConfig);
 export const auth = app.auth();
 export const database = app.database();
+export const storage = app.storage();
