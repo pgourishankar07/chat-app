6db47733d4de8893372b3c36b7a26e38c03ce3e6
pgourishankar07 <100337318+pgourishankar07@users.noreply.github.com>
2023-07-27 21:12:58 +0530

    Chat room design

--- src/pages/Home/Chat.jsx
+++ src/pages/Home/Chat.jsx
@@ -1,3 +1,35 @@
+import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
+import Bottom from '../../components/chat-window/bottom';
+import Messages from '../../components/chat-window/messages';
+import Top from '../../components/chat-window/top';
+import { useRooms } from '../../context/room.context';
+import { Loader } from 'rsuite';
+
 export default function Chat() {
-  return <div>Chat</div>;
+  const { chatId } = useParams();
+  const rooms = useRooms();
+
+  if (!rooms) {
+    return <Loader center vertical size="md" content="Loading" speed="slow" />;
+  }
+
+  const currRoom = rooms.find(room => room.id === chatId);
+
+  if (!currRoom) {
+    return <h6 className="text-center mt-page">Chat {chatId} not found</h6>;
+  }
+
+  return (
+    <>
+      <div className="chat-top">
+        <Top />
+      </div>
+      <div className="chat-middle">
+        <Messages />
+      </div>
+      <div className="chat-bottom">
+        <Bottom />
+      </div>
+    </>
+  );
 }
