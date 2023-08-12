964bf3a53e482f2625d46bc6e38ed76cf2eb957c
pgourishankar07 <100337318+pgourishankar07@users.noreply.github.com>
2023-08-12 23:04:19 +0530

    Code splitting

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
+import React, { Suspense } from 'react';
 import { Switch, BrowserRouter } from 'react-router-dom';
 
 import 'rsuite/dist/styles/rsuite-default.css';
 import './styles/main.scss';
-import SignIn from './pages/SignIn';
+// import SignIn from './pages/SignIn';
 import Home from './pages/Home';
 import PrivateRoute from './components/PrivateRoute';
 import PublicRoute from './components/PublicRoute';
 import { ProfileContextProvider } from './context/profile.context';
 import { ErrorBoundary } from './components/ErrorBoundary';
+
+const SignIn = React.lazy(() => import('./pages/SignIn'));
 
 function App() {
   return (
@@ -16,7 +19,9 @@ function App() {
         <BrowserRouter>
           <Switch>
             <PublicRoute path="/signin">
-              <SignIn />
+              <Suspense fallback={<div>Loading...</div>}>
+                <SignIn />
+              </Suspense>
             </PublicRoute>
             <PrivateRoute path="/">
               <Home />
